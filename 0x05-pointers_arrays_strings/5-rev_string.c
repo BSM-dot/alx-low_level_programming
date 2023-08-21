5730e5ba302c58ecdc01588a6352068289f5f47a
Bongani <bsmthimkhulu213@gmail.com>
2023-08-21 06:09:56 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,26 +8,8 @@
   */
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
+  int len = 0, index = 0;
+  char tmp;
 
-	a = s;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
-}
+  while
+      

8656f1f1fe3fef2686ea9fa23c28a4022aea07bb
Bongani <bsmthimkhulu213@gmail.com>
2023-08-21 05:42:55 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @str: String to count
+ *
+ * Return: The length of @str
+ */
+size_t _strlen(const char *str)
+{
+size_t length = 0;
+
+while (*str++)
+lenght++;
+
+return (length);
+}
